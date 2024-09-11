1cc532d6eb4d77183a499813c3b610475ed83a54
Johannes <johannes-programming@mailfence.com>
2024-09-11 19:53:05 +0200

    Version 1.0.4

--- src/seqpad/__init__.py
+++ src/seqpad/__init__.py
@@ -1,17 +1,28 @@
-import wonderparse as _wp
+import click as _click
+import getoptify as _getoptify
 
 
-def seqpad(seq:"nucleotide sequence"):
+def seqpad(seq):
     while len(seq) % 3 != 0:
-        seq += 'N'
+        seq += "N"
     return seq
 
-def main(args=None):
-    _wp.easymode.simple_run(
-        args=args,
-        program_object=seqpad,
-        prog='seqpad',
-    )
-    
-if __name__ == '__main__':
-    main() 
+
+@_getoptify.command(
+    shortopts="hV",
+    longopts=["help", "version"],
+    allow_argv=True,
+    gnu=True,
+)
+@_click.command(add_help_option=False)
+@_click.help_option("-h", "--help")
+@_click.version_option(None, "-V", "--version")
+@_click.argument("seq", required=False)
+def main(seq):
+    """pad seq to a length that is a multiple of three"""
+    if seq is not None:
+        _click.echo(seqpad(seq))
+
+
+if __name__ == "__main__":
+    main()
