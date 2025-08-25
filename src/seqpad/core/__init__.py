b4ea6038f3395726e02c498d08fbe9527b21117f
Johannes <johannes.programming@gmail.com>
2025-08-25 20:44:54 +0200

    Version 1.0.12+local

--- src/seqpad/core/__init__.py
+++ src/seqpad/core/__init__.py
@@ -6,18 +6,18 @@ import preparse
 __all__ = ["seqpad", "main"]
 
 
-def seqpad(seq):
+def seqpad(seq: Any) -> Any:
     while len(seq) % 3 != 0:
         seq += "N"
     return seq
 
 
-@preparse.PreParser(posix=False).click()
+@preparse.PreParser().click()
 @click.command(add_help_option=False)
 @click.help_option("-h", "--help")
 @click.version_option(None, "-V", "--version")
-@click.argument("seq", required=False)
-def main(seq):
-    """pad seq to a length that is a multiple of three"""
+@click.argument("seq", type=str, required=False)
+def main(seq: Optional[str]) -> None:
+    "This program pads the given seq to a length that is a multiple of three."
     if seq is not None:
         click.echo(seqpad(seq))
